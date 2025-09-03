d7e20639a4afa320c58e70b99de352faed2e7b28
cesaraha <c.hdez.alvz@gmail.com>
2025-09-03 09:47:23 +0200

    Remove build
    Removed unnecessary build property.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // Use '/' since this is your main GitHub Pages repo
-  build: {
-    outDir: 'dist'
-  }
 })
